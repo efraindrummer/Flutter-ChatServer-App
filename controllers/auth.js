5878eae422e355ec828d64b96083ea1076299e60
Efrain May <efrain_may_75@hotmail.com>
2021-01-27 20:27:33 -0600

    grabar usuario en base de datos

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,14 @@
 const { response } = require("express");
+const Usuario = require('../models/usuario');
 
+const crearUsuario = async (req, res = response) => {
 
-const crearUsuario = (req, res = response) => {
+    const usuario = new Usuario(req.body);
+    await usuario.save();
 
     res.json({
         ok: true,
-        msg: 'Crear usuario!!!'
+        body: req.body,
     })
 }
 
---
+++ models/usuario.js
@@ -0,0 +1,23 @@
+const { Schema, model} = require('mongoose');
+
+const UsuarioSchema = Schema({
+    nombre: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    online: {
+        type: Boolean,
+        default: false
+    },
+});
+
+module.exports = model('Usuario', UsuarioSchema);
