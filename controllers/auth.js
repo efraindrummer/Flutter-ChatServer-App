1e12571cf8b0bda35e71f2c4e111d77984a9e135
Efrain May <efrain_may_75@hotmail.com>
2021-01-27 21:01:59 -0600

    contraseña encriptada

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,42 @@
 const { response } = require("express");
+const bcrypt = require('bcryptjs');
 const Usuario = require('../models/usuario');
 
 const crearUsuario = async (req, res = response) => {
 
-    const usuario = new Usuario(req.body);
-    await usuario.save();
+    const { email, password } = req.body;
 
-    res.json({
-        ok: true,
-        body: req.body,
-    })
+    try {
+
+        const existeEmail = await Usuario.findOne({email});
+        if(existeEmail){
+            return res.status(400).json({
+                ok: false,
+                msg: 'El correo ya esta registrado'
+            });
+        }
+
+        const usuario = new Usuario(req.body);
+
+        //Encriptar contraseña
+        const salt = bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync( password, salt);
+
+        await usuario.save();
+
+        res.json({
+            ok: true,
+            usuario
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+    }
+
 }
 
 module.exports = {
--- models/usuario.js
+++ models/usuario.js
@@ -1,3 +1,4 @@
+const { JsonWebTokenError } = require('jsonwebtoken');
 const { Schema, model} = require('mongoose');
 
 const UsuarioSchema = Schema({
@@ -18,6 +19,13 @@ const UsuarioSchema = Schema({
         type: Boolean,
         default: false
     },
+});
+
+UsuarioSchema.method('toJSON', function(){
+    const { __v, _id, password, ...object} = this.toObject();
+    object.uid = _id;
+    return object;
+    //el object se llama cuando lo enviamos al json para mantener le estado actual de la aplicacion
 });
 
 module.exports = model('Usuario', UsuarioSchema);
