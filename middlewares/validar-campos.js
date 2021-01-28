28180cbe6b608f80ef0f083b3daff85090b46878
Efrain May <efrain_may_75@hotmail.com>
2021-01-27 20:19:46 -0600

    validaciones con express validator y middleware para validar campos

---
+++ middlewares/validar-campos.js
@@ -0,0 +1,20 @@
+const { validationResult } = require("express-validator");
+
+
+const validarCampos = (req, res, next) => {
+
+    const errores = validationResult(req);
+
+    if(!errores.isEmpty()){
+        return res.status(400).json({
+            ok: false,
+            errores: errores.mapped()
+        })
+    }
+
+    next();
+}
+
+module.exports = {
+    validarCampos
+}
--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,17 @@
     path: api/login
 */
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { crearUsuario } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
 const router = Router();
 
-router.post('/new', crearUsuario);
+router.post('/new', [
+    check('nombre','El nombre es obligatorio').not().isEmpty(),
+    check('password','La contraseña es obligatoria').not().isEmpty(),
+    check('email','El email es obligatorio').isEmail(),
+    validarCampos
+],crearUsuario);
 
 
 module.exports = router;
